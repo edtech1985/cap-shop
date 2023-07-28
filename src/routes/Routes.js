9f87703c8714b872a33bd4deed89c1922a334f17
Edson Costa <98822745+edtech85@users.noreply.github.com>
2023-07-28 13:13:20 -0300

    first merge

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styled from "styled-components";
 
 export const Main = styled.main`
@@ -7,13 +6,3 @@ export const Main = styled.main`
   flex-direction: column;
   justify-content: space-between;
 `;
-=======
-import styled from "styled-components";
-
-export const Main = styled.main`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
->>>>>>> 4b68a1092258c410e9a8e8767c0325f0786973e0
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
@@ -27,33 +26,3 @@ export default function AppRouter() {
     </Main>
   );
 }
-=======
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import Home from "../pages/Home";
-import Checkout from "../pages/Checkout";
-import CapsMale from "../pages/Caps/CapsMale";
-import CapsFemale from "../pages/Caps/CapsFemale";
-import { Main } from "./Routes";
-import { CartProvider } from "../components/CartContext";
-
-export default function AppRouter() {
-  return (
-    <Main>
-      <Router>
-        <CartProvider>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/caps/male" element={<CapsMale />} />
-            <Route path="/caps/female" element={<CapsFemale />} />
-          </Routes>
-          <Footer />
-        </CartProvider>
-      </Router>
-    </Main>
-  );
-}
->>>>>>> 4b68a1092258c410e9a8e8767c0325f0786973e0
