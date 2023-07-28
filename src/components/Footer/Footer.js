14f4c430847f945aef6037dc6d2e4a8c29a037bb
Edson Costa <98822745+edtech85@users.noreply.github.com>
2023-07-28 13:24:46 -0300

    solve conflicts

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
@@ -42,48 +41,3 @@ export const StyledDevelopedBy = styled.p`
   margin: 0;
   font-size: 0.9rem;
 `;
-=======
-import styled from "styled-components";
-
-export const StyledFooter = styled.footer`
-  background-color: black;
-  color: white;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem;
-  width: 100%;
-  max-width: 100vw;
-  box-sizing: border-box;
-`;
-
-export const StyledIconList = styled.ul`
-  display: flex;
-  margin: 0;
-  padding: 0;
-`;
-
-export const StyledIconItem = styled.li`
-  margin-right: 1rem;
-  list-style-type: none;
-  font-size: 1.5rem;
-  cursor: pointer;
-  &:hover {
-    color: gray;
-  }
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-`;
-
-export const StyledRightsReserved = styled.p`
-  margin: 0;
-  font-size: 0.9rem;
-`;
-
-export const StyledDevelopedBy = styled.p`
-  margin: 0;
-  font-size: 0.9rem;
-`;
->>>>>>> 4b68a1092258c410e9a8e8767c0325f0786973e0
