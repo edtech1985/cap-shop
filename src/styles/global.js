0079fe72385cdbb98890d6f8de39d5b51cfee7ef
Edson Costa <98822745+edtech85@users.noreply.github.com>
2023-07-28 13:21:17 -0300

    solve conflicts

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
@@ -30,36 +29,3 @@ body {
 `;
 
 export default GlobalStyle;
-=======
-import { createGlobalStyle } from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
-
-* {
-    margin: 0;
-    padding: 0;
-    font-family: sans-serif;
-
-}
-
-body {
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    background-color: #f2f2f2;
-}
-
-:root {
-  --green: #6da544;
-  --blue: #27aee1;
-  --yellow: #ffda44;
-  --default-blue: #03aeeb;
-  --default-green: #6bda55;
-  --default-yellow: #d7df23;
-}
-
-`;
-
-export default GlobalStyle;
->>>>>>> 4b68a1092258c410e9a8e8767c0325f0786973e0
